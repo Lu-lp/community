55c4c8852aa031bdbf04c2701f8c9a71dc984e68
Lu-lp <15626259445@163.com>
2021-06-07 17:11:30 +0800

    init repo

---
+++ src/test/java/sysu/lulp/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package sysu.lulp.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
